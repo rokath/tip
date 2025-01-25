9688d15db5002820078eb3730411e5e410cc2175
Thomas Höhenleitner <th@seerose.net>
2025-01-26 00:28:30 +0100

    Update tip.go

--- tip/cmd/tip/tip.go
+++ tip/cmd/tip/tip.go
@@ -1,11 +1,12 @@
 package main
 
+import(
+    "encoding/hex"
+	"github.com/rokath/tip/pkg/tip"
+	)
+
 func main() {
-
-}
-
-func tiPack(in []byte) (out []byte) {
-	maxLen := 8*len(in)/7 + 1 // if no compression is possible, for each byte 1 more bit is needed
-	out = make([]byte, 0, maxLen)
-	return out
+	in := []byte{0x01, 0x01, 0x01, 0x01}
+	out := tip.Pack(in)
+	fmt.Println(hex.Dump(out))
 }
