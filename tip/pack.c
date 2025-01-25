84487a9b715aa9051eccac4478a589e639f51f18
Thomas Höhenleitner <th@seerose.net>
2025-01-25 07:01:38 +0100

    Update pack.c

--- tip/pack.c
+++ tip/pack.c
@@ -10,49 +10,46 @@ typedef struct {
     uint8_t  by; // by is the replacement byte 0x01 to 0xff.
 } replacement_t;
 
-//! @brief rpp is the replacement list. It cannot get more elements.
+//! @brief rp is the replacement list. It cannot get more elements.
+//! The space between 2 rps is a hay stack.
 static replacement_t rp[TIP_SRC_BUFFER_SIZE_MAX/2];
 
 //! @brief rpInit is called when a new unpacked buffer arrived.
-void rpInit( void ){
+void rpInit(size_t len){
     // The first 2 elements are initialized as boders.
-    rp[0].bo = 0; // From idx to rp[0].bo is first hey stack.
-    rp[0].sz = 0; // Dummy entry to get started.
-    // The space between 2 rps is a hay stack.
-    rp[1].bo = TIP_SRC_BUFFER_SIZE_MAX; // From idx to rp[0].bo is first hey stack.
-  //rp[1].sz = 0; // Dummy entry to get started.
-}
+    rp[0].bo = 0;
+    rp[0].sz = 0; 
+    // From (rp[0].bo + rp[0].sz) to rp[1].bo is the first hey stack.
+    rp[1].bo = len;
+};
 
 //! @brief tip encodes src buffer with size len into dst buffer and returns encoded len.
 //! @details For the tip encoding it uses the linked tipTable.c object.
 size_t tip( uint8_t* dst, uint8_t const * src, size_t len ){
-    rpInit();
+    rpInit(len);
     for( int i = 0; i < TipTableLength; i++ ){
         // get next needle (the next pattern)
         uint8_t * needle = TipTable[i].pt;
         size_t nlen = TipTable[i].sz;
-        // Traverse the already found replacements to find hey stacks to look for the needle.
+        // Traverse rp to find hey stacks.
         int k = 0;
         do{      
             // get next hay stack
-            uint8_t hay = src + rp[k].bo + rp[k].sz;
+            uint8_t const * hay = src + rp[k].bo + rp[k].sz;
             size_t hlen = rp[k+1].bo - rp[k].bo - rp[k].sz;
             // search the needle
             uint8_t * loc = memmem( hay, hlen, needle, nlen );
-            if( loc ){
-                uint8_t by = TipTable[i].by;
+            if( loc ){ // found
+                uint8_t by = TipTable[i].by; // by is the replacement byte.
                 uint16_t offset = loc - src; // offset is the needle (=pattern) position.
                 rpInsert( by, offset, nlen );
-                k++; // The rp insert takes part inside the already processed rps.
             }
-            k++;
+            k++; // The rp insert takes part inside the already processed rps.
         }while(hay+hlen<src+len)
     }
-
     // Some bytes groups in the src buffer are replacable with 0x01...0xFF and some not.
     // The replacement list rp contains now the replacement information.
     // Lets collect the unreplacable bytes into a buffer now.
-
     collectUnreplacableBytes( src );
     convertUnreplacableBytes();
     return generateTipPacket( dst );
