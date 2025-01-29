c16a85d0a5fe3a419fe5b93be339d4cf5ebad899
Thomas Höhenleitner <th@seerose.net>
2025-01-29 23:37:34 +0100

    Update tip_test.go

--- pkg/tip/tip_test.go
+++ pkg/tip/tip_test.go
@@ -12,8 +12,17 @@ func TestBuffer(t *testing.T) {
 	buf := make([]byte, 100)
 	n := Pack(buf, in)
 
+	assertNoZeroes(t, buf)
+	
 	out := make([]byte, 100)
 	m := Unpack(out, buf[:n])
 	
 	assert.Equal(t, in, out[:m])
 }
+
+func assertNoZeroes(t *testing.T, b []byte) {
+	for x := range b{
+		assert.NotEqual(t,x,0)
+	}
+}
+	
