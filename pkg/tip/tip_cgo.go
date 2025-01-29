09bb9bc8516382ddb12d46a570429f4b23a03e95
Thomas Höhenleitner <th@seerose.net>
2025-01-29 23:30:13 +0100

    Update tip_cgo.go

--- pkg/tip/tip_cgo.go
+++ pkg/tip/tip_cgo.go
@@ -1,13 +1,12 @@
 // Copyright 2025 Thomas.Hoehenleitner [at] seerose.net
 // Use of this source code is governed by a license that can be found in the LICENSE file.
 
-// Package tip_cgo is a helper for testing the target C-code functions.
+// Package tip_cgo is a helper for accessing the target C-code functions.
 // Each C function to test gets a Go wrapper which is tested in appropriate test functions.
 // For some reason inside the tip_test.go an 'import "C"' is not possible.
 
 // The Go functions defined here are not exported. They are called by the Go test functions in this package.
 // This way the test functions are executing the tip C-code compiled with the tipConfig.h here.
-// Inside ./testdata this file is named cgoPackage.go where it is maintained.
 package tip
 
 // #cgo CFLAGS: -g -Wall -I../../src
@@ -20,6 +19,8 @@ import (
 	"unsafe"
 )
 
+
+// See also tiptable.SliceIndex function.
 // memMem returns position of needle in hay or -1.
 func memMem(hay, needle []byte) (pos int) {
 	h := (*C.uchar)(unsafe.Pointer(&hay[0]))
