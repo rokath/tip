cecfc9f869052b274f61965868cfdcd8c4672e0e
Thomas Höhenleitner <th@seerose.net>
2025-02-01 00:40:15 +0100

    wip

--- src/memmem.c
+++ src/memmem.c
@@ -1,6 +1,7 @@
 //! @file memmem.c
 //! @brief From // https://stackoverflow.com/questions/2188914/c-searching-for-a-string-in-a-file
 
+#include <stdint.h>
 #include <string.h>
 #include "memmem.h"
 
@@ -31,4 +32,21 @@ void *memmem(const void *haystack, size_t hlen, const void *needle, size_t nlen)
         plen = hlen - (p - haystack);
     }
     return NULL;
+}
+
+/** 
+ * @brief The memmemOffset() function finds the start of the first occurrence of the
+ * substring 'needle' of length 'nlen' in the memory area 'haystack' of
+ * length 'hlen'.
+ *
+ * @details The return value is the offset from the beginning of the sub-string, or
+ * -1 if the substring is not found.
+ */
+int MemmemOffset(const void *haystack, size_t hlen, const void *needle, size_t nlen){
+    uint8_t* pos = memmem(haystack, hlen, needle, nlen);
+    if(pos == NULL){
+        return -1;
+    }
+    int offset = pos - (uint8_t*)(haystack);
+    return offset;
 }
--- src/memmem.h
+++ src/memmem.h
@@ -4,3 +4,4 @@
 #include <stddef.h>
 
 void *memmem(const void *haystack, size_t hlen, const void *needle, size_t nlen);
+int MemmemOffset(const void *haystack, size_t hlen, const void *needle, size_t nlen);
