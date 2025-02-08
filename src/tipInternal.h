8d4d712e216753d989f4d314b3945aaa6dba0ee1
Thomas Höhenleitner <th@seerose.net>
2025-02-08 08:17:38 +0100

    Update tipInternal.h

--- src/tipInternal.h
+++ src/tipInternal.h
@@ -60,17 +60,10 @@ typedef struct {
     uint8_t * last; //! last is the last address inside the unreplacable bytes buffer. ( = &(buffer[sizeof(buffer)-1]); )
 } unreplacable_t;
 
-/*
 typedef struct {
-    uint8_t const byte;
-} pattern_t;
-
-typedef struct {
-    pattern_t * pattern;
+    uint8_t const * const pattern;
     size_t const size;
 } idTable_t;
-*/
-
 
 void getPatternFromId( uint8_t id, uint8_t ** pt, size_t * sz );
 void restartPattern(void);
