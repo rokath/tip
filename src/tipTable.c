14a203534424f0f7d7a537cf08bb8b92a9410cb6
Thomas Höhenleitner <th@seerose.net>
2025-02-08 08:28:38 +0100

    Update tipTable.c

--- src/tipTable.c
+++ src/tipTable.c
@@ -6,7 +6,7 @@
 
 //! tipTable is sorted by pattern count and pattern length.
 //! The pattern position + 1 is the replacement id.
-uint8_t idTable[] = { // from trice.bin (2025-01-15 21:45)                                                                        -- __ASCII__          |  count  id
+uint8_t pattern[] = { // from trice.bin (2025-01-15 21:45)                                                                        -- __ASCII__          |  count  id
 	  2, 0xff, 0xff,                                                                                                             //                      |  17140  01
 	  3, 0xff, 0xff, 0xff,                                                                                                       //                      |  11812  02
 	  4, 0xff, 0xff, 0xff, 0xff,                                                                                                 //                      |   6854  03
@@ -137,4 +137,9 @@ uint8_t idTable[] = { // from trice.bin (2025-01-15 21:45)                      
 	  0 // table end marker
 };
 
-const size_t tipTableSize = 847;
+const size_t idTableSize = 847;
+
+idTable_t idTable = {
+    &pattern;
+    idTableSize;
+};
