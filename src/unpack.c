f2de8fcca57bda1b68305ff4ac26a76610abab28
Thomas Höhenleitner <th@seerose.net>
2025-02-11 18:36:27 +0100

    wip

--- src/unpack.c
+++ src/unpack.c
@@ -16,6 +16,29 @@ size_t tiUnpack( uint8_t* dst, const uint8_t * table, const uint8_t * src, size_
     if( slen < 6 ){
         memcpy(dst, src, slen);
         return slen;
-    }   
+    }
+    static uint8_t u7[256]; // todo
+    size_t u7len = collectU7Bytes( u7, src, slen );
+
+    static uint8_t u8[256]; // todo
+    size_t u8len = shift78bit( u8, u7, u7len );
+
+    size_t dlen = restorePacket( dst, table, u8, u8len, src, slen );
+    return dlen;
+}
+
+// collectU7Bytes copies all bytes with msbit=1 into dst and returns their count.
+size_t collectU7Bytes( uint8_t * dst, uint8_t * src, size_t slen ){
+    uint8_t * p = dst;
+    for( int i = 0; i < slen; i++ ){
+        if(0x80 & src[i]){
+            *p++ = src[i];
+        }
+    }
+    return p - dst;
+}
+
+//! restorePacket reconstructs original data using src, slen, u8, u8len and table into dst and returns the count.
+size_t restorePacket( uint8_t * dst, uint8_t * table, uint8_t u8, size_t u8len, uint8_t * src, size_t slen ){
     return 0; // todo
 }
