00e1aed485297a7b195952a5ce03dafbeaa5dbf9
Thomas Höhenleitner <th@seerose.net>
2025-03-06 13:09:55 +0100

    wip, 1-sided ok, 2-sided buggy

--- src/pack.c
+++ src/pack.c
@@ -14,7 +14,6 @@
 #include "memmem.h"
 
 /*static*/ replace_t * buildReplaceList(int * rcount, const uint8_t * table, const uint8_t * src, size_t slen);
-/*static*/ replace_t * buildReplaceList2(int * rcount, const uint8_t * table, const uint8_t * src, size_t slen);
 static size_t collectUnreplacableBytes( uint8_t * dst, replace_t * rlist, int rcount, const uint8_t * src );
 /*static*/ size_t shift87bit( uint8_t* lst, const uint8_t * src, size_t slen );
 static void initGetNextPattern( const uint8_t * table );
@@ -37,7 +36,7 @@ size_t tiPack( uint8_t* dst, const uint8_t * table, const uint8_t * src, size_t 
         return 0;
     }
     int rcount;
-    replace_t * rlist = buildReplaceList2(&rcount, table, src, slen);
+    replace_t * rlist = buildReplaceList(&rcount, table, src, slen);
     // All unreplacable bytes are stretched inside to 7-bit units. This makes the data a bit longer.
     static uint8_t ur[TIP_SRC_BUFFER_SIZE_MAX*8u/7u+1]; 
     size_t ubSize = collectUnreplacableBytes( ur, rlist, rcount, src );
@@ -46,74 +45,6 @@ size_t tiPack( uint8_t* dst, const uint8_t * table, const uint8_t * src, size_t 
     uint8_t * u7 = urLimit - urSize;
     size_t tipSize = generateTipPacket( dst, u7, urSize, rlist, rcount );
     return tipSize;
-}
-
-// buildReplaceList starts with biggest table pattern and searches for matches.
-/*static*/ replace_t * buildReplaceList(int * rcount, const uint8_t * table, const uint8_t * src, size_t slen){
-    replace_t * rlist = newReplaceList(slen);
-    *rcount = 2;
-    initGetNextPattern(table);
-    for( int id = 1; id < 0x80; id++ ){ // traverse the ID table.
-        // get biggest needle (the next pattern)
-        const uint8_t * needle = NULL;
-        size_t nlen;
-        getNextPattern( &needle, &nlen );
-        if( nlen == 0 ){ // end of table if less 127 IDs
-            break; 
-        }
-        // Traverse r to find hey stacks.
-        int k = 0;
-        const uint8_t * hay;
-        size_t hlen;
-        do{ // get next hay stack
-            repeat:
-            hay = src + rlist[k].bo + rlist[k].sz;
-            hlen = rlist[k+1].bo - rlist[k].bo - rlist[k].sz;  
-            uint8_t * loc = memmem( hay, hlen, needle, nlen ); // search hay for the needle
-            if( loc ){ // found, id is the replace byte.
-                offset_t offset = loc - src; // offset is the relative needle (=pattern) position.
-                replaceableListInsert( rlist, rcount, k, id, offset, nlen );
-                goto repeat; // Same k and same  (needle) needs processing again but in the next hay stack.
-            } // The r insert takes part inside the already processed rs.
-            k++;
-        }while( hay+hlen < src+slen );
-    }
-    return rlist;
-}
-
-// buildReplaceList2 starts with src and tries to find biggest matching pattern from table at buf = src.
-// If one was found, buf is incremented by found pattern size and we start over.
-// If none found, we increment buf by 1 and start over.
-/*static*/ replace_t * buildReplaceList2(int * rcount, const uint8_t * table, const uint8_t * src, size_t slen){
-    replace_t * rlist = newReplaceList(slen);
-    *rcount = 2;
-    int k = 0;
-    const uint8_t * buf = src;
-repeat:
-    initGetNextPattern(table);
-    for( int id = 1; id < 0x80; id++ ){ // traverse the ID table.    
-        // get the next pattern
-        const uint8_t * needle = NULL;
-        size_t nlen;
-        getNextPattern( &needle, &nlen );
-        if( nlen == 0 ){ // end of table if less 127 IDs
-            break; 
-        }
-        if( strncmp((void*)buf, (void*)needle, nlen) ){ // no match
-            continue; // with next pattern
-        }
-        // found, id is the replace byte.
-        offset_t offset = buf - src; // relative pattern position
-        replaceableListInsert( rlist, rcount, k, id, offset, nlen );
-        k++;
-        buf += nlen;
-        goto repeat;
-    }
-    buf++;
-    if( buf < src + slen ){
-        goto repeat;
-    }
-    return rlist;
 }
 
 static const uint8_t * nextTablePos = 0;
@@ -255,3 +186,165 @@ static size_t collectUnreplacableBytes( uint8_t * dst, replace_t * rlist, int rc
     }
     return lst - dst;
 }
+
+#if 0 // 0 side
+
+// buildReplaceList starts with biggest table pattern and searches for matches.
+replace_t * buildReplaceList(int * rcount, const uint8_t * table, const uint8_t * src, size_t slen){
+    replace_t * rlist = newReplaceList(slen);
+    *rcount = 2;
+    initGetNextPattern(table);
+    for( int id = 1; id < 0x80; id++ ){ // traverse the ID table.
+        // get biggest needle (the next pattern)
+        const uint8_t * needle = NULL;
+        size_t nlen;
+        getNextPattern( &needle, &nlen );
+        if( nlen == 0 ){ // end of table if less 127 IDs
+            break; 
+        }
+        // Traverse r to find hey stacks.
+        int k = 0;
+        const uint8_t * hay;
+        size_t hlen;
+        do{ // get next hay stack
+            repeat:
+            hay = src + rlist[k].bo + rlist[k].sz;
+            hlen = rlist[k+1].bo - rlist[k].bo - rlist[k].sz;  
+            uint8_t * loc = memmem( hay, hlen, needle, nlen ); // search hay for the needle
+            if( loc ){ // found, id is the replace byte.
+                offset_t offset = loc - src; // offset is the relative needle (=pattern) position.
+                replaceableListInsert( rlist, rcount, k, id, offset, nlen );
+                goto repeat; // Same k and same  (needle) needs processing again but in the next hay stack.
+            } // The r insert takes part inside the already processed rs.
+            k++;
+        }while( hay+hlen < src+slen );
+    }
+    return rlist;
+}
+
+#endif
+
+#if 1 // 1 side
+
+// buildReplaceList starts with src and tries to find biggest matching pattern from table at buf = src.
+// If one was found, buf is incremented by found pattern size and we start over.
+// If none found, we increment buf by 1 and start over.
+replace_t * buildReplaceList(int * rcount, const uint8_t * table, const uint8_t * src, size_t slen){
+    replace_t * rlist = newReplaceList(slen);
+    *rcount = 2;
+    int k = 0;
+    const uint8_t * buf = src;
+repeat:
+    initGetNextPattern(table);
+    for( int id = 1; id < 0x80; id++ ){ // traverse the ID table.    
+        // get the next pattern
+        const uint8_t * needle = NULL;
+        size_t nlen;
+        getNextPattern( &needle, &nlen );
+        if( nlen == 0 ){ // end of table if less 127 IDs
+            break; 
+        }
+        if( strncmp((void*)buf, (void*)needle, nlen) ){ // no match
+            continue; // with next pattern
+        }
+        // found, id is the replace byte.
+        offset_t offset = buf - src; // relative pattern position
+        replaceableListInsert( rlist, rcount, k, id, offset, nlen );
+        k++;
+        buf += nlen;
+        goto repeat;
+    }
+    buf++;
+    if( buf < src + slen ){
+        goto repeat;
+    }
+    return rlist;
+}
+
+#endif
+
+#if 0 // 2 sides
+
+// replaceableListIndex checks. if at offset a pattern with size sz is already known with id.
+// If so, -1 is returned.
+// Otherwise the k is returned after which the insertion is ok.
+static int replaceableListIndex(replace_t * rlist, int * rcount, uint8_t id, offset_t offset, uint8_t sz){
+    for( int k = 1; k < *rcount; k++ ){
+        if( rlist[k].bo < offset ){
+            continue;
+        }
+        if( rlist[k].bo == offset ){
+            //while( rlist[k].sz != sz );
+            //while( rlist[k].id != id );
+            return -1;
+        }
+        return k-1; // rlist[k].bo > offset
+    }
+    //for(;;);
+    return -1;
+}
+
+// buildReplaceList starts with src and tries to find biggest matching pattern from table at buf = src.
+// If one was found, buf is incremented by found pattern size and we start over.
+// If none found, we increment buf by 1 and start over.
+/*static*/ replace_t * buildReplaceList(int * rcount, const uint8_t * table, const uint8_t * src, size_t slen){
+    replace_t * rlist = newReplaceList(slen);
+    *rcount = 2;
+    int k = 0;
+    const uint8_t * buf = src;
+    const uint8_t * bufLimit = src + slen;
+    int startSearch = 1;
+    int endSearch = 1;
+repeat:
+    initGetNextPattern(table);
+    for( int id = 1; id < 0x80; id++ ){ // traverse the ID table.    
+        // get the next pattern
+        const uint8_t * needle = NULL;
+        size_t nlen;
+        getNextPattern( &needle, &nlen );
+        if( nlen == 0 ){ // end of table if less 127 IDs
+            break; 
+        }
+        if( startSearch && 0 == strncmp((void*)buf, (void*)needle, nlen) ){ // match at buf start  
+            offset_t offset = buf - src; // relative pattern position
+            k = replaceableListIndex( rlist, rcount, id, offset, nlen );
+            if( k != -1 ){
+                replaceableListInsert( rlist, rcount, k, id, offset, nlen ); // id is the replace byte.
+                //k++;
+            }
+            buf += nlen;
+            if( !(buf < src + slen)){
+                startSearch = 0;
+            }
+            goto repeat;
+        }
+        if( endSearch && 0 == strncmp((void*)(bufLimit-nlen), (void*)needle, nlen) ){ // match at buf end
+            offset_t offset = bufLimit-nlen - src; // relative pattern position
+            k = replaceableListIndex( rlist, rcount, id, offset, nlen );
+            if( k != -1 ){
+                replaceableListInsert( rlist, rcount, k, id, offset, nlen ); // id is the replace byte.
+                //k++;
+            }
+            bufLimit -= nlen;
+            if( !(src < bufLimit) ){
+                endSearch = 0;
+            }
+            goto repeat;            
+        }
+        continue; // with next pattern  
+    }
+    buf++;
+    bufLimit--;
+    if( !(buf < src + slen)){
+        startSearch = 0;
+    }
+    if( !(src < bufLimit) ){
+        endSearch = 0;
+    }
+    if( startSearch || endSearch ){
+        goto repeat;
+    }
+    return rlist;
+}
+
+#endif
