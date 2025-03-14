0c220adbe222f71880c3333c8ac0a9c3753b9c96
Thomas Höhenleitner <th@seerose.net>
2025-03-14 07:26:40 +0100

    Update tipConfig.h

--- src/tipConfig.h
+++ src/tipConfig.h
@@ -13,6 +13,7 @@ extern "C" {
 #define TIP_SRC_BUFFER_SIZE_MAX 200u // 65528u // bytes (max 65528u)
 
 //! MAX_PATH_COUNT is the max allowed path count.
+//! Its size has influence on the statically allocated RAM.
 #define MAX_PATH_COUNT 20
 
 #ifdef __cplusplus
